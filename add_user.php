3d47c6ccd556265a506b13bf6b0351d72435d15d
Utkarsh Agarwal <coolguy.agarwal365@gmail.com>
2017-02-16 03:43:41 +0530

    Added POST validation

--- add_user.php
+++ add_user.php
@@ -2,13 +2,21 @@
 //session_start();
 //if(isset($_SESSION["user"]) && !empty($_SESSION["user"]))
 //{
+
+
+if ($_SERVER['REQUEST_METHOD'] != 'POST' || !isset($_POST['secret']))
+{
+	echo "Some error occured"; 
+	die();
+}
+
 $nm=$_POST['name'];
 $rg=$_POST['reg'];
 $coll=$_POST['coll'];
 $em=$_POST['email'];
 $gn=$_POST['gender'];
 $ct=$_POST['cont'];
-
+$usr=1;
 
 
 $connection=mysqli_connect("localhost","phpmyadmin","utkarsh_123","sports_portal");
@@ -18,11 +26,11 @@ $coll=mysqli_real_escape_string($connection,$coll);
 $em=mysqli_real_escape_string($connection,$em);
 $gn=mysqli_real_escape_string($connection,$gn);
 $ct=mysqli_real_escape_string($connection,$ct);
-
+$usr=mysqli_real_escape_string($connection,$usr);
 
 $query ="INSERT INTO
-		 delegate_reg(Name,Reg_num,College,Email,Phone,Gender) VALUES
-		 ('$nm', '$rg', '$coll', '$em', $ct, '$gn');";
+		 delegate_reg(Name,Reg_num,College,Email,Phone,Gender,U_id) VALUES
+		 ('$nm', '$rg', '$coll', '$em', '$ct', '$gn','$usr');";
 $result=mysqli_query($connection,$query);
 
 if(!$result)
@@ -33,7 +41,9 @@ else
 {
 $id=mysqli_insert_id($connection);
 echo "<h2>User successfully registered with id ".$id."</h2>";
+echo "<pre>";
 print_r($_POST);
+echo "</pre>";
 }
 mysqli_close($connection);
 //}
